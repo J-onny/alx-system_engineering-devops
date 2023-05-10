1ced6b7b1f4d933581fd69010103aaa807c212f6
j-onny <omo.pro22@gmail.com>
2023-05-11 02:44:57 +0300

    wbstck3

---
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -0,0 +1,9 @@
+# Script to fix apache error
+
+exec { '/var/www/html/wp-setting.php':
+  command => "sed -i 's/.phpp/.php/g' /var/www/html/wp-settings.php",
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin
+:/usr/bin:/sbin:/bin',
+}
+
+
