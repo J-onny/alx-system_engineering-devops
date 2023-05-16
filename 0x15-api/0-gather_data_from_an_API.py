6c3f2dba47858efa09a794432a0a68a6f8e16a81
j-onny <omo.pro22@gmail.com>
2023-05-16 15:10:54 +0300

    api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,24 +1,16 @@
 #!/usr/bin/env python3
+
 from requests import get
 from sys import argv
-
-
-
-
 if __name__ == '__main__':
-    user_id = sys.argv[1]
-    url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
-    response = requests.get(url)
-    employee_name = response.json().get('name')
-    url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)
-    response = requests.get(url)
-    tasks = response.json()
-    completed_tasks = [task for task in tasks if task.get('completed')]
-    num_completed_tasks = len(completed_tasks)
-    total_num_tasks = len(tasks)
+    user_id = argv[1]                                     url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)                                       response = get(url)                                   name = response.json().get('name')                                                                          url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)                                 response = get(url)
+    tasks = response.json()                               done = 0                                              done_tasks = []
+    for task in tasks:
+        if task.get('completed'):
+            done_tasks.append(task)
+            done += 1
 
-    print("Employee {} is done with tasks ({}/{}):"
-          .format(employee_name, num_completed_tasks, total_num_tasks))
-    for task in completed_tasks:
-        print("\t{} {}".format('\u2022', task.get('title')))
-
+    print("Employee {} is done with tasks({}/{}):"
+          .format(name, done, len(tasks)))
+    for task in done_tasks:
+        print("\t {}".format(task.get('title')))
--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -1,36 +1,8 @@
 #!/usr/bin/python3
-import csv
-import requests
-import sys
 
-if __name__ == '__main__':
-    
-    user_id = sys.argv[1]
-
-    user_url = f'https://jsonplaceholder.typicode.com/users/{user_id}'
-    user_response = requests.get(user_url)
-    user_data = user_response.json()
-
-    tasks_url = f'https://jsonplaceholder.typicode.com/users/{user_id}/todos'
-    tasks_response = requests.get(tasks_url)
-    tasks_data = tasks_response.json()
-
-    employee_name = user_data['name']
-    filename = f'{user_id}.csv'
-
-    with open(filename, mode='w', newline='') as csv_file:
-        fieldnames = ['USER_ID', 'USERNAME', 'TASK_COMPLETED_STATUS', 'TASK_TITLE']
-        writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
-
-        writer.writeheader()
-
-        for task in tasks_data:
-            writer.writerow({
-                'USER_ID': user_id,
-                'USERNAME': employee_name,
-                'TASK_COMPLETED_STATUS': str(task['completed']),
-                'TASK_TITLE': task['title']
-            })
-
-    print(f'TODO list progress for employee {employee_name} has been exported to {filename}')
-
+from requests import get
+from sys import argv
+                                                                                                            if __name__ == '__main__':                                user_id = argv[1]                                     url = 'https://jsonplaceholder.typicode.com/users/{}'.format(user_id)
+    response = get(url)                                   username = response.json().get('username')                                                                  url = 'https://jsonplaceholder.typicode.com/users/{}/todos'.format(user_id)                                 response = get(url)                                   tasks = response.json()                               with open('{}.csv'.format(user_id), 'w') as file:         for task in tasks:                                        file.write('"{}","{}","{}","{}"\n'
+                       .format(user_id, username, task.get('completed'),
+                               task.get('title')))
